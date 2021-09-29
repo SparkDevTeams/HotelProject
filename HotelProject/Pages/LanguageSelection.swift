3e2346c08f87ecb8724f08f1a25f945ab2a6a600
VincentCarrancho <71295634+VincentCarrancho@users.noreply.github.com>
2021-09-29 17:25:38 -0400

    Added language selection page. READ TODO on line 33.

--- HotelProject/Pages/LanguageSelection.swift
+++ HotelProject/Pages/LanguageSelection.swift
@@ -9,7 +9,34 @@ import SwiftUI
 
 struct LanguageSelection: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            Text("Select your language:")
+                .font(.system(size: 48))
+                .fontWeight(.medium)
+            languageBtn(languageLabel: "English")
+            languageBtn(languageLabel: "Spanish")
+            languageBtn(languageLabel: "French")
+            languageBtn(languageLabel: "Mandarin")
+            
+        }
+        
+    }
+}
+
+
+struct languageBtn: View {
+    var languageLabel: String
+    
+    
+    var body: some View {
+        NavigationLink(destination: {
+            Text(languageLabel == "English" ? "English" : "Language not yet supported")
+            // !
+            // !TODO: replace first ternary "English" (after ?) with another SwiftUI View
+            // !
+        }, label: {Text(languageLabel).font(.system(size: 36)).foregroundColor(Color.black)})
+            .padding()
     }
 }
 
