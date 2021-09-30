4ddbc7488dc30443b0720548b26bcd643a12a6e6
josepht419 <josephtt419@gmail.com>
2021-09-30 12:47:46 -0400

    Room Selection

---
+++ HotelProject/Pages/Room Selection/RoomSelection.swift
@@ -0,0 +1,20 @@
+//
+//  RoomSelection.swift
+//  HotelProject
+//
+//  Created by Joseph Torres on 9/30/21.
+//
+
+import SwiftUI
+
+struct RoomSelection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RoomSelection_Previews: PreviewProvider {
+    static var previews: some View {
+        RoomSelection()
+    }
+}
