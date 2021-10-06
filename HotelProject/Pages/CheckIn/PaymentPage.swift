1547037ac5892852963c0848e392a264d5c25a6d
David Barcenas <dabarcenas6921@gmail.com>
2021-10-06 17:42:34 -0400

    Added payment page

---
+++ HotelProject/Pages/CheckIn/PaymentPage.swift
@@ -0,0 +1,123 @@
+//
+//  PaymentPage.swift
+//  HotelProject
+//
+//  Created by David Barcenas on 10/4/21.
+//
+
+import SwiftUI
+
+// change
+
+struct PaymentPage: View {
+    
+    @State var ifComplete: Bool = false
+    @State var amountOfNights: String = ""
+    @State var email: String = ""
+    @State var confirmEmail: String = ""
+    @State var cardNumber: String = ""
+    @State var CVC: String = ""
+    @State var cardholderName: String = ""
+    @State var locationName: String = ""
+    @State var zipCode: String = ""
+    @State var expirationDate = Date()
+    
+    var body: some View {
+        VStack{
+            
+            PaymentPageTextfieldView(amountOfNights: $amountOfNights, email: $email, confirmEmail: $confirmEmail, cardNumber: $cardNumber, CVC: $CVC, cardholderName: $cardholderName, locationName: $locationName, zipCode: $zipCode, expirationDate: $expirationDate)
+            
+            Button {
+                ifComplete = true
+            } label: {
+                Text("Next")
+                    .font(.system(size: 24))
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 100)
+                    .background(
+                        Color.black
+                            .cornerRadius(20))
+                    .padding()
+            }
+
+            
+        }
+    }
+}
+
+struct PaymentPageTextfieldView: View {
+    
+    @Binding var amountOfNights: String
+    @Binding var email: String
+    @Binding var confirmEmail: String
+    @Binding var cardNumber: String
+    @Binding var CVC: String
+    @Binding var cardholderName: String
+    @Binding var locationName: String
+    @Binding var zipCode: String
+    @Binding var expirationDate: Date
+
+    
+    var body: some View{
+        VStack{
+            Text("Your total is $xxx.xx for a " +  amountOfNights + " night.")
+                .font(.system(size: 48))
+                .fontWeight(.medium)
+                .padding()
+            
+            TextFieldComponent(sfSymbol: "envelope.fill", labelString: "Email", input: email)
+                
+            TextFieldComponent(sfSymbol: "envelope.fill", labelString: "Confirm email", input: confirmEmail)
+            
+            Text("Payment Information")
+                .font(.system(size: 30))
+                .fontWeight(.medium)
+            
+            TextFieldComponent(sfSymbol: "creditcard.fill", labelString: "Card number", input: cardNumber)
+            
+            HStack{
+                
+                DatePicker(
+                    "Expiration Date",
+                    selection: $expirationDate,
+                    displayedComponents: [.date]
+                )   .frame(height:70)
+                    .padding()
+                    .datePickerStyle(.compact).overlay(RoundedRectangle(
+                    cornerRadius: 15)
+                    .stroke(Color.black, lineWidth: lineWidth))
+                    .padding()
+                    
+                
+                TextField("CVC: ", text:$CVC)
+                    .frame(height: 70)
+                    .padding(15)
+                        .overlay(RoundedRectangle(
+                            cornerRadius: 15).stroke(Color.black, lineWidth: lineWidth))
+                        .padding()
+                    
+                
+            }.frame(width:totalWidth)
+            
+            TextFieldComponent(sfSymbol: "person.crop.circle.fill", labelString: "Name on Card", input: cardholderName)
+            
+            Text("Country or Region")
+                .font(.system(size: 30))
+                .fontWeight(.medium)
+            
+            TextFieldComponent(sfSymbol: "mappin.and.ellipse", labelString: "Country/Region", input: locationName)
+            
+            TextFieldComponent(sfSymbol: "mappin.and.ellipse", labelString: "Zip Code", input: locationName)
+            
+        }
+    }
+}
+
+
+
+struct PaymentPage_Previews: PreviewProvider {
+    static var previews: some View {
+        PaymentPage()
+    }
+}
